2c64b1313f6d2ef5574a5a0d704831c9b025ea75
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-06-04 14:55:32 +0200

    node-hw_1 complete

---
+++ project/index.js
@@ -0,0 +1,48 @@
+const { program } = require("commander");
+
+const contactsOperations = require("./contacts");
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await contactsOperations.listContacts();
+      console.log("contacts :>> ", contacts);
+      break;
+
+    case "get":
+      const contact = await contactsOperations.getContactById(id);
+      console.log("contact :>> ", contact);
+      if (!contact) {
+        throw new Error(`Product with id=${id} not found!`);
+      }
+      break;
+
+    case "add":
+      const addContact = await contactsOperations.addContact(
+        name,
+        email,
+        phone
+      );
+      console.log("addContact :>> ", addContact);
+      break;
+
+    case "remove":
+      const removeContact = await contactsOperations.removeContact(id);
+      console.log("removeContact :>> ", removeContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+const options = program.opts();
+invokeAction(options);
